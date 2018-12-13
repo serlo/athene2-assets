d477b5a4e8bfa73c894fe015890581f61de8bd5a
Jonas Keinholz <jonas@keinholz.com>
2018-12-13 15:03:01 +0100

    fix tests

--- src/server-side-rendering/index.tsx
+++ src/server-side-rendering/index.tsx
@@ -1,6 +1,6 @@
-import * as express from 'express'
-import * as cors from 'cors'
-import * as bodyParser from 'body-parser'
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 import * as ReactDOMServer from 'react-dom/server'
 import * as React from 'react'
 
@@ -9,8 +9,6 @@ interface Registry {
 }
 
 export const createApp = (registry: Registry) => {
-  console.log(express)
-
   const app = express()
 
   app.use(cors())
