029648e5f8928ec04c26c4fc359853401bb4fc86
Jonas Keinholz <git@inyono.dev>
2019-06-04 13:54:29 +0200

    chore: yarn format

--- scripts/changelog.ts
+++ scripts/changelog.ts
@@ -142,7 +142,7 @@ const releases: Parameters<typeof generateChangelog>['0'] = [
     tagName: 'v4.0.1',
     name: '4.0.1',
     date: '2019-06-04',
-    fixed: ['Show datetimes correctly'],
+    fixed: ['Show datetimes correctly']
   }
 ]
 
